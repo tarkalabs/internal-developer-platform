60fce1d9a2ae530b4383c1d03fa04080ef2a4d1d
Madhava Reddy SV <madhav@tarkalabs.com>
2023-03-24 12:34:50 -0300

    New product config validate script update

---
+++ scripts/new-product-validate.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+require 'json'
+
+puts "Predefined template provided: #{ARGV[0]}"
+
+puts "Frontend application(s) JSON provided: #{ARGV[1]}"
+
+puts "Backend application(s) JSON provided: #{ARGV[2]}"
